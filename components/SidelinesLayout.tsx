a4cde9a2fdb821bddb6d8a90eca5dd46f14f07f0
zen <zenon.deviagge@gmail.com>
2025-09-30 18:43:19 -0300

    lines too thick, doesnt extend thorughout whole app

--- components/SidelinesLayout.tsx
+++ components/SidelinesLayout.tsx
@@ -14,22 +14,24 @@ export default function SidelinesLayout({
   className = '',
 }: SidelinesLayoutProps) {
   return (
-    <div className={`bg-white ${className}`}>
-      <div className="flex justify-center min-h-screen">
-        {/* Left Sideline with diagonal stripes */}
-        <div className="hidden xl:flex w-[160px] flex-shrink-0 sideline-pattern">
-          <div className="sticky top-[180px] p-4 w-full">
-            {/* Ads will be hidden but pattern visible */}
+    <div className={`bg-white min-h-full ${className}`}>
+      <div className="flex justify-center">
+        {/* Left Sideline */}
+        <div className="hidden xl:flex w-[160px] flex-shrink-0">
+          <div className="w-full sideline-pattern">
+            <div className="sticky top-[180px] p-4">{/* Ads hidden */}</div>
           </div>
         </div>
 
-        {/* Main Content Area - FIXED WIDTH */}
-        <div className="w-[1200px] bg-white shadow-sm">{children}</div>
+        {/* Main Content */}
+        <div className="w-[1200px] bg-white shadow-sm min-h-full">
+          {children}
+        </div>
 
-        {/* Right Sideline with diagonal stripes */}
-        <div className="hidden xl:flex w-[160px] flex-shrink-0 sideline-pattern">
-          <div className="sticky top-[180px] p-4 w-full">
-            {/* Ads will be hidden but pattern visible */}
+        {/* Right Sideline */}
+        <div className="hidden xl:flex w-[160px] flex-shrink-0">
+          <div className="w-full sideline-pattern">
+            <div className="sticky top-[180px] p-4">{/* Ads hidden */}</div>
           </div>
         </div>
       </div>
