a034dfaadba81b85ebbcc85349e5741019d9e090
zen <zenon.deviagge@gmail.com>
2025-10-15 09:55:31 -0300

    reduce h2 in PrincipalSection

--- components/PrincipalSection.tsx
+++ components/PrincipalSection.tsx
@@ -490,7 +490,7 @@ return (
                   </div>
                 </div>
                 <div className="p-4 pt-2 flex-1 flex flex-col justify-start">
-                  <h2 className="text-lg font-bold leading-tight">
+                  <h2 className="text-base font-bold leading-tight">
                     {article.overline && (
                       <span className="text-primary-red">
                         {article.overline}.{' '}
@@ -538,7 +538,7 @@ return (
                   </div>
                 </div>
                 <div className="p-4 pt-2 flex-1 flex flex-col justify-start">
-                  <h2 className="text-lg font-bold leading-tight">
+                  <h2 className="text-base font-bold leading-tight">
                     {article.overline && (
                       <span className="text-primary-red">
                         {article.overline}.{' '}
