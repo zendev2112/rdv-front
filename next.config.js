28a2f99a08afa8f35c5645ff82b9e2e4d0223211
zen <zenon.deviagge@gmail.com>
2025-11-27 14:04:32 -0300

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
