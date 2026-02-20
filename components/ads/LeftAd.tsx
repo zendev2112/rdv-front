c8c7d235c9b6fc9fd4b0bc98c34024745bdab7e3
zen <zenon.deviagge@gmail.com>
2026-02-20 14:37:17 -0300

    update ad

--- components/ads/LeftAd.tsx
+++ components/ads/LeftAd.tsx
@@ -1,7 +1,7 @@
 import AdBanner from './AdBanner'
 
 const AD_URL =
-  'https://res.cloudinary.com/dptdloagw/image/upload/v1771608172/Agregar_un_t%C3%ADtulo_otoo4l.gif'
+  'https://res.cloudinary.com/dptdloagw/image/upload/v1771608955/Agregar_un_t%C3%ADtulo_pzy1fm.png'
 
 export default function LeftAd() {
   return (
